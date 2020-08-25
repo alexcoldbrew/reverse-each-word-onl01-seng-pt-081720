48a242b9444e74d7c4e51cd497cc59f3cce7dc91
Alex Sanchez <alexanderclaysanchez@gmail.com>
2020-08-25 00:52:33 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,15 @@
 def reverse_each_word(sentence)
+  # first, turn the string into an array by splitting at each space
   array = sentence.split(" ")
+  # create empty array to put the reversed into
   return_array = []
+  # iterate over the new array by reversing 1st with .reverse and shovel on
   array.each do|sentence|
     return_array << sentence.reverse
   end
+  # return the reversed array by turning it into a string with .join, adding space at index
   return_array.join(" ")
 end
+
+# now, do the same thing with .collect/.map instead of .each
 
